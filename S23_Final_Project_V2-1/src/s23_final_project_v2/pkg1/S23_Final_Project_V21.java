dc6634ba4216909213c1a9a001ed95d6a6668961
diramim <diramim@GSS130024TI.dsbnac.edu.on.ca>
2023-05-24 11:25:39 -0400

    Initial Netbeans project setup

---
+++ S23_Final_Project_V2-1/src/s23_final_project_v2/pkg1/S23_Final_Project_V21.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s23_final_project_v2.pkg1;
+
+/**
+ *
+ * @author diramim
+ */
+public class S23_Final_Project_V21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Testing 123");
+    }
+    
+}
